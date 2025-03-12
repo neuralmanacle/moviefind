47b4b48b62579ad9cf01536a6be08e4366f1bb4b
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-03-13 03:06:26 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Search from './Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="w-100 main-wrapper d-flex flex-column justify-content-center align-items-center">
+      <header className="w-100 main-wrapper">
+        <h2>Find Your Movie</h2>
+        <Search />
       </header>
+      
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+        <input type="text" name="movie" className='w-50 p-2' placeholder='Search for a movie' />
+    </div>
+  )
+}
+
+export default Search
